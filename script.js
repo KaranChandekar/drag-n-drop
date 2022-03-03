551acd7a75a188ee1cb6eedc9492391398a9363e
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-03 21:33:40 +0530

    adding all other functions

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ for(const empty of empties) {
     empty.addEventListener('dragleave', dragLeave)
     empty.addEventListener('drop', dragDrop)
 }
+
+function dragStart() {
+    this.className += ' hold' 
+    setTimeout(() => this.className = 'invisible', 0)
+}
+
+function dragEnd() {
+    this.className = 'fill'
+}
+
+function dragOver(e) {
+    e.preventDefault()
+}
+
+function dragEnter(e) {
+    e.preventDefault()
+    this.className += ' hovered'
+}
+
+function dragLeave() {
+    this.className = 'empty'
+}
+
+function dragDrop() {
+    this.className = 'empty'
+    this.append(fill)
+}
