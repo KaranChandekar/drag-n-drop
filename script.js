c541064e8d8d2b00ff47eb2d2ec2239e1337aa9b
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-03 21:32:28 +0530

    add eventlisteners on fill

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const fill = document.querySelector('.fill')
 const empties = document.querySelectorAll('.empty')
+
+fill.addEventListener('dragstart', dragStart)
+fill.addEventListener('dragend', dragEnd)
